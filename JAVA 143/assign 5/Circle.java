61f862f69b080480f085c97ffdee20bee20b5b6e
Robert Wood Jr <rdrwood@gmail.com>
2018-05-09 23:19:03 -0700

    homework
    homework to turn in

--- JAVA 143/assign 5/Circle.java
+++ JAVA 143/assign 5/Circle.java
@@ -1,52 +1,50 @@
 
-
 /*
   file: Circle.java
   author: Robert Wood
   CPW 143 homework assignment 5
-  Description: This code ---------------------------------------------------------
-  ******************************************************
-  *
-  **
-  *
+  Description: This code returns the information requested from the shapetester class
   
-  *
-  
-  *
-  --------------------------------------------------------
-  .
 */
 import java.util.*;
-
-
 public class Circle implements Shape{
       
    Point center;
    int radius;                             
-
-
+    // no arg constructor
    public Circle(){
       this.center = center;
       this.radius = radius;
    }
-   
+       // general constructor
    public Circle (Point center, int radius){
       this.center = center;
       this.radius = radius;   
    }   
-   
+
+   // gets and calculates area
    public double getArea(){
       return (Math.PI * (radius * radius));
    }
-   
+  
+   /**
+   * caclulate the distance around the circle.
+   * @return the perimeter of the circle.
+   */
    public double getPerimeter(){
       return ((radius * 2) * Math.PI);
    }
    
+   /*
+   * Java standard toString() method
+   */
    public String toString(){
       return "{" + center + ", " + radius + "}" ;
    }
    
+   /**
+   * Java standard equals( Object ) method
+   */
    public boolean equals( Object other ){
        if ( other == null ) {
           return false;
@@ -55,17 +53,9 @@ public class Circle implements Shape{
            Circle c = (Circle) other;
            if(c.center == this.center && c.radius == this.radius){
                 return true;
-           }
-                 
-                                      
+           }                       
        }
-       return false;
-
-
-                    
-       
+       return false;         
    }
-   
-   
 }
 
--- JAVA 143/assign 5/Rectangle.java
+++ JAVA 143/assign 5/Rectangle.java
@@ -4,10 +4,8 @@
   file: Circle.java
   author: Robert Wood
   CPW 143 homework assignment 5
-  Description: This code ---------------------------------------------------------
-  ******************************************************
-  --------------------------------------------------------
-  .
+  Description: This code returns the information requested from the shapetester class
+  
 */
 
 import java.util.*;
@@ -17,21 +15,21 @@ public class Rectangle implements Shape{
     Point p2;  // has 
     Point p3;  // four
     Point p4;  // corners.
-
+    // no arg constructor
    public Rectangle(){
         this( new Point(0, 0),
               new Point(1, 1),
               new Point(2, 5),
               new Point(3, 6) );
    }
-   
+       // general constructor
    public Rectangle (Point p1, Point p2, Point p3,  Point p4){
         this.p1 = p1;
         this.p2 = p2;
         this.p3 = p3;  
         this.p4 = p4;
    }   
-   
+   // gets and calculates area
    public double getArea(){
       double sideA = p1.distance( p2 );
       double sideB = p2.distance( p3 );
@@ -39,16 +37,24 @@ public class Rectangle implements Shape{
       return area;
    }
    
+   /**
+   * caclulate the distance around the rect.
+   * @return the perimeter of the rect.
+   */
    public double getPerimeter(){
       return ((p1.distance( p2 ) + p2.distance( p3 ) ) * 2 );
    }
    
+   /*
+   * Java standard toString() method
+   */
    public String toString(){
       return "{" + p1 + ", " + p2 + ", " + p3 + ", " + p4 + "}" ;
    }
    
-   
-   
+   /**
+   * Java standard equals( Object ) method
+   */
    public boolean equals( Object other ){
        if ( other == null ) {
           return false;
@@ -60,14 +66,12 @@ public class Rectangle implements Shape{
                 || line.p1.equals( p4 ) && line.p2.equals( p2 ) && line.p3.equals( p3 ) && line.p4.equals( p1 )
                 || line.p1.equals( p4 ) && line.p2.equals( p3 ) && line.p3.equals( p2 ) && line.p4.equals( p1 )
                 
-                /*|| line.p1.equals( p2 ) && line.p2.equals( p1 ) && line.p3.equals( p3 )
-                || line.p1.equals( p2 ) && line.p2.equals( p3 ) && line.p3.equals( p1 )
-                || line.p1.equals( p3 ) && line.p2.equals( p1 ) && line.p3.equals( p3 )
-                || line.p1.equals( p3 ) && line.p2.equals( p2 ) && line.p3.equals( p1 )*/;
+                || line.p1.equals( p2 ) && line.p2.equals( p1 ) && line.p3.equals( p3 ) && line.p4.equals( p4 )
+                || line.p1.equals( p2 ) && line.p2.equals( p1 ) && line.p3.equals( p4 ) && line.p4.equals( p3 )
+                || line.p1.equals( p3 ) && line.p2.equals( p2 ) && line.p3.equals( p1 ) && line.p4.equals( p4 )
+                || line.p1.equals( p3 ) && line.p2.equals( p4 ) && line.p3.equals( p1 ) && line.p4.equals( p2 );
         }
         return false;
-
    }
-   
 }
 
