20b297e4f3bb111ec232402991ecee0e5294aef4
Robert Wood Jr <rdrwood@gmail.com>
2018-05-23 12:19:50 -0700

    added comments

--- JAVA 143/Assignments/assign 7/Calculator.java
+++ JAVA 143/Assignments/assign 7/Calculator.java
@@ -2,6 +2,11 @@ import java.util.*;
 
 /**
  * TODO: Javadoc comment here about Calculator class
+ * This class allows a user to enter 2 operands and one operator and perform a math calculation.
+ * The class contains all of the code for functionality and does not reference any other sources.
+ * @author Ken M
+ * @author for the try catch and java doc comments Robert M. Wood Jr
+ * @version 1.1 ( May 23 2018)
  */
 public class Calculator {
 
@@ -43,7 +48,7 @@ public class Calculator {
             char operator = '&';
             boolean isValid = true;
             
-            // TODO: needs try/catch block
+            // added try/catch block here
             try{
   	             firstOperand = Integer.parseInt( tokens[ 0 ] );
             }
@@ -51,7 +56,8 @@ public class Calculator {
             	 System.out.println("Number format exception on first operand");
                  isValid = false;
             }
-// commented out to test with      firstOperand = Integer.parseInt( tokens[ 0 ] );
+            // commented out to test with      firstOperand = Integer.parseInt( tokens[ 0 ] );
+            // previous line was original code commented and left for testing sake.
             
             // we set value of isValid to false
             // whenever we get invalid input.
@@ -62,7 +68,7 @@ public class Calculator {
                 isValid = false;
             }
             
-            // TODO:needs try/catch block
+            // added try/catch block here
             try{
                 secondOperand = Integer.parseInt( tokens[ 2 ] );
             }
@@ -111,7 +117,9 @@ public class Calculator {
     }
     
     /**
-     * TODO: Java doc comment here
+     * Java doc comment inserted here
+     * The following code displayInstructions when called will display the instructions for the class.
+     * In this case it will print 6 lines explaining how to use the calculator correctly.
      */
     public static void displayInstructions() {
         System.out.println( "This program evaluates binary arithmetic" );
@@ -119,7 +127,6 @@ public class Calculator {
         System.out.println( "integers and one of the operators + - * /");
         System.out.println( "Put one space between each item in the" );
         System.out.println( "expression. Example: 2 + 3" );
-        System.out.println( "To end the calculations, type "
-                          +  STOP_STRING );
+        System.out.println( "To end the calculations, type " +  STOP_STRING );
     }
 }
